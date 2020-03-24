d119579e6aae02ee764bb826a481de2b791f3107
Kapil <kapilkumar2428@gmail.com>
2020-03-24 23:37:14 +0530

    Adding new Project

---
+++ Photos/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Krithika Iyer 2 on 24/03/20.
+//  Copyright © 2020 kapil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello world")
+        print("Hello world")
+    }
+
+
+}
+
